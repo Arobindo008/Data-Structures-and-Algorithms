334c300718b8d3d547cbf72f8c050e434edfbfa9
arobindo sarkar <arobindosarkar9118@gmail.com>
2026-05-16 17:26:03 +0600

    practice:medium array

---
+++ medium array/majorityElement.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int majorityElement(vector<int> arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+                count++;
+        }
+        if (count > n / 2)
+            return arr[i];
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {1, 1, 2, 1, 3, 5, 1};
+    cout << majorityElement(arr) << endl;
+}
