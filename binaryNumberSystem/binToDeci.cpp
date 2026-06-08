b925d50a646a6a1ac9a265bac3a3075dcca699ab
arobindo sarkar <arobindosarkar9118@gmail.com>
2026-06-08 20:12:29 +0600

    practice: bin num sys

---
+++ binaryNumberSystem/binToDeci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int convert(int n)
+{
+    int ans = 0, pow = 1;
+    while (n > 0)
+    {
+        int rem = n % 10;
+        ans += (rem * pow);
+        n /= 10;
+        pow *= 2;
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << convert(n) << endl;
+}
