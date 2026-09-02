99ea71b50e208cc2e3b6e3206300bfde83d52a7e
arobindo sarkar <arobindosarkar9118@gmail.com>
2026-09-02 19:02:59 +0600

    binary search

--- binary search/binSearch.cpp
+++ binary search/binSearch.cpp
@@ -2,27 +2,35 @@
 #include <vector>
 using namespace std;
 
-int binSearch(vector<int> arr,int target){
-    int st=0,end=arr.size()-1;
-    while(st<=end){
-        int mid=(st+end)/2;
-        if(target>arr[mid]){
-            st=mid+1;
+int binSearch(vector<int> arr, int target)
+{
+    int st = 0, end = arr.size() - 1;
+    while (st <= end)
+    {
+        int mid = (st + end) / 2;
+
+        if (target > arr[mid])
+        {
+            st = mid + 1;
         }
-        else if(target<arr[mid]){
-            end=mid-1;
+
+        else if (target < arr[mid])
+        {
+            end = mid - 1;
         }
-        else 
-           return mid;
+
+        else
+            return mid;
     }
-    
+
     return -1;
 }
 
 int main()
 {
-    vector<int> arr={-1,0,3,4,5,7,12};
-    int target=15;
-    cout<<binSearch(arr,target)<<endl;
+    vector<int> arr = {-1, 0, 3, 4, 5, 7, 12};
+    int target = 15;
+
+    cout << binSearch(arr, target) << endl;
     return 0;
 }
